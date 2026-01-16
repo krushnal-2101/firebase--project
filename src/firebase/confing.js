891c86f881799086e639cc1da015ab306bf07542
Krushnal Bhatti <krushnalbhatti71@gmail.com>
2026-01-16 12:58:18 +0530

    Add files via upload

---
+++ src/firebase/confing.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDfQNGkBeYHkHvm8OTnOywvbbd8x7MEXYU",
+  authDomain: "fir-project-7bd1d.firebaseapp.com",
+  projectId: "fir-project-7bd1d",
+  storageBucket: "fir-project-7bd1d.firebasestorage.app",
+  messagingSenderId: "1023499487673",
+  appId: "1:1023499487673:web:6ae3702ee9ce0dc5724e68"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
